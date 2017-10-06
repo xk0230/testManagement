ccf308086c35d43c13e8dd8fc240c7802c6053a9
xiaokan <xiaokan@192.168.56.1>
2017-10-07 02:00:56 +0800

    添加总评胜任特征

---
+++ src/com/codyy/oc/admin/dao/CandidateRRecrcomMapper.java
@@ -0,0 +1,17 @@
+package com.codyy.oc.admin.dao;
+
+import com.codyy.oc.admin.entity.CandidateRRecrcom;
+
+public interface CandidateRRecrcomMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(CandidateRRecrcom record);
+
+    int insertSelective(CandidateRRecrcom record);
+
+    CandidateRRecrcom selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(CandidateRRecrcom record);
+
+    int updateByPrimaryKey(CandidateRRecrcom record);
+}
---
+++ src/com/codyy/oc/admin/entity/CandidateRRecrcom.java
@@ -0,0 +1,43 @@
+package com.codyy.oc.admin.entity;
+
+public class CandidateRRecrcom {
+    private String id;
+
+    private String candidateId;
+
+    private String recRComId;
+
+    private String value;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+
+    public String getRecRComId() {
+        return recRComId;
+    }
+
+    public void setRecRComId(String recRComId) {
+        this.recRComId = recRComId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
