97387e10a2caf6e2729c6a3d97b938011eaed45f
hk5249 <hk5249@ly.com>
2017-10-16 16:20:34 +0800

    添加图表年份选择查询

---
+++ src/com/codyy/oc/admin/vo/CostYearVO.java
@@ -0,0 +1,29 @@
+
+package com.codyy.oc.admin.vo;
+
+import java.util.List;
+
+public class CostYearVO {
+    
+    private int curYear;;
+    
+    private List<Integer> years;
+
+    public int getCurYear() {
+        return curYear;
+    }
+
+    public void setCurYear(int curYear) {
+        this.curYear = curYear;
+    }
+
+    public List<Integer> getYears() {
+        return years;
+    }
+
+    public void setYears(List<Integer> years) {
+        this.years = years;
+    }
+    
+}
+
