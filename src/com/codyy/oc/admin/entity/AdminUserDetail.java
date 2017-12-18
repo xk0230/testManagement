df6f4392a0beb7b4cf587f856d404b8c93ba5f28
xiaokan <xiaokan@DESKTOP-KNHNPB3>
2017-12-18 21:13:16 +0800

    添加User_detail字段

--- src/com/codyy/oc/admin/entity/AdminUserDetail.java
+++ src/com/codyy/oc/admin/entity/AdminUserDetail.java
@@ -61,9 +61,548 @@ public class AdminUserDetail {
     
     private String sj;
     
+    private String english;
 
+    private String otherLanguage;
+
+    private String address;
+
+    private Date firstWorkTime;
+
+    private String homeName;
+
+    private String homeXl;
+
+    private String homeZy;
+
+    private Date homeBirthday;
+
+    private String homeWork;
+
+    private String homeZw;
+
+    private String homePhone;
+
+    private String homeIdcard;
+
+    private String child1Name;
+
+    private Date child1Birthday;
+
+    private String child1Sex;
+
+    private String child1Work;
+
+    private String child2Name;
+
+    private Date child2Birthday;
+
+    private String child2Sex;
+
+    private String child2Work;
+
+    private String child3Name;
+
+    private Date child3Birthday;
+
+    private String child3Sex;
+
+    private String child3Work;
+
+    private String work1Time;
+
+    private String work1Place;
+
+    private String work1Reason;
+
+    private String work1Job;
+
+    private String work1Zyyj;
+
+    private String work2Time;
+
+    private String work2Place;
+
+    private String work2Reason;
+
+    private String work2Job;
+
+    private String work2Zyyj;
+
+    private String work3Time;
+
+    private String work3Place;
+
+    private String work3Reason;
+
+    private String work3Job;
+
+    private String work3Zyyj;
+
+    private String work4Time;
+
+    private String work4Place;
+
+    private String work4Reason;
+
+    private String work4Job;
+
+    private String work4Zyyj;
+
+    private String work5Time;
+
+    private String work5Place;
+
+    private String work5Reason;
+
+    private String work5Job;
+
+    private String work5Zyyj;
+
+    private String work6Time;
+
+    private String work6Place;
+
+    private String work6Reason;
+
+    private String work6Job;
+
+    private String work6Zyyj;
+    
     public String getPdpSjA() {
 		return pdpSjA;
+	}
+
+	public String getEnglish() {
+		return english;
+	}
+
+	public void setEnglish(String english) {
+		this.english = english;
+	}
+
+	public String getOtherLanguage() {
+		return otherLanguage;
+	}
+
+	public void setOtherLanguage(String otherLanguage) {
+		this.otherLanguage = otherLanguage;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getFirstWorkTime() {
+		return firstWorkTime;
+	}
+
+	public void setFirstWorkTime(Date firstWorkTime) {
+		this.firstWorkTime = firstWorkTime;
+	}
+
+	public String getHomeName() {
+		return homeName;
+	}
+
+	public void setHomeName(String homeName) {
+		this.homeName = homeName;
+	}
+
+	public String getHomeXl() {
+		return homeXl;
+	}
+
+	public void setHomeXl(String homeXl) {
+		this.homeXl = homeXl;
+	}
+
+	public String getHomeZy() {
+		return homeZy;
+	}
+
+	public void setHomeZy(String homeZy) {
+		this.homeZy = homeZy;
+	}
+
+	public Date getHomeBirthday() {
+		return homeBirthday;
+	}
+
+	public void setHomeBirthday(Date homeBirthday) {
+		this.homeBirthday = homeBirthday;
+	}
+
+	public String getHomeWork() {
+		return homeWork;
+	}
+
+	public void setHomeWork(String homeWork) {
+		this.homeWork = homeWork;
+	}
+
+	public String getHomeZw() {
+		return homeZw;
+	}
+
+	public void setHomeZw(String homeZw) {
+		this.homeZw = homeZw;
+	}
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+	public String getHomeIdcard() {
+		return homeIdcard;
+	}
+
+	public void setHomeIdcard(String homeIdcard) {
+		this.homeIdcard = homeIdcard;
+	}
+
+	public String getChild1Name() {
+		return child1Name;
+	}
+
+	public void setChild1Name(String child1Name) {
+		this.child1Name = child1Name;
+	}
+
+	public Date getChild1Birthday() {
+		return child1Birthday;
+	}
+
+	public void setChild1Birthday(Date child1Birthday) {
+		this.child1Birthday = child1Birthday;
+	}
+
+	public String getChild1Sex() {
+		return child1Sex;
+	}
+
+	public void setChild1Sex(String child1Sex) {
+		this.child1Sex = child1Sex;
+	}
+
+	public String getChild1Work() {
+		return child1Work;
+	}
+
+	public void setChild1Work(String child1Work) {
+		this.child1Work = child1Work;
+	}
+
+	public String getChild2Name() {
+		return child2Name;
+	}
+
+	public void setChild2Name(String child2Name) {
+		this.child2Name = child2Name;
+	}
+
+	public Date getChild2Birthday() {
+		return child2Birthday;
+	}
+
+	public void setChild2Birthday(Date child2Birthday) {
+		this.child2Birthday = child2Birthday;
+	}
+
+	public String getChild2Sex() {
+		return child2Sex;
+	}
+
+	public void setChild2Sex(String child2Sex) {
+		this.child2Sex = child2Sex;
+	}
+
+	public String getChild2Work() {
+		return child2Work;
+	}
+
+	public void setChild2Work(String child2Work) {
+		this.child2Work = child2Work;
+	}
+
+	public String getChild3Name() {
+		return child3Name;
+	}
+
+	public void setChild3Name(String child3Name) {
+		this.child3Name = child3Name;
+	}
+
+	public Date getChild3Birthday() {
+		return child3Birthday;
+	}
+
+	public void setChild3Birthday(Date child3Birthday) {
+		this.child3Birthday = child3Birthday;
+	}
+
+	public String getChild3Sex() {
+		return child3Sex;
+	}
+
+	public void setChild3Sex(String child3Sex) {
+		this.child3Sex = child3Sex;
+	}
+
+	public String getChild3Work() {
+		return child3Work;
+	}
+
+	public void setChild3Work(String child3Work) {
+		this.child3Work = child3Work;
+	}
+
+	public String getWork1Time() {
+		return work1Time;
+	}
+
+	public void setWork1Time(String work1Time) {
+		this.work1Time = work1Time;
+	}
+
+	public String getWork1Place() {
+		return work1Place;
+	}
+
+	public void setWork1Place(String work1Place) {
+		this.work1Place = work1Place;
+	}
+
+	public String getWork1Reason() {
+		return work1Reason;
+	}
+
+	public void setWork1Reason(String work1Reason) {
+		this.work1Reason = work1Reason;
+	}
+
+	public String getWork1Job() {
+		return work1Job;
+	}
+
+	public void setWork1Job(String work1Job) {
+		this.work1Job = work1Job;
+	}
+
+	public String getWork1Zyyj() {
+		return work1Zyyj;
+	}
+
+	public void setWork1Zyyj(String work1Zyyj) {
+		this.work1Zyyj = work1Zyyj;
+	}
+
+	public String getWork2Time() {
+		return work2Time;
+	}
+
+	public void setWork2Time(String work2Time) {
+		this.work2Time = work2Time;
+	}
+
+	public String getWork2Place() {
+		return work2Place;
+	}
+
+	public void setWork2Place(String work2Place) {
+		this.work2Place = work2Place;
+	}
+
+	public String getWork2Reason() {
+		return work2Reason;
+	}
+
+	public void setWork2Reason(String work2Reason) {
+		this.work2Reason = work2Reason;
+	}
+
+	public String getWork2Job() {
+		return work2Job;
+	}
+
+	public void setWork2Job(String work2Job) {
+		this.work2Job = work2Job;
+	}
+
+	public String getWork2Zyyj() {
+		return work2Zyyj;
+	}
+
+	public void setWork2Zyyj(String work2Zyyj) {
+		this.work2Zyyj = work2Zyyj;
+	}
+
+	public String getWork3Time() {
+		return work3Time;
+	}
+
+	public void setWork3Time(String work3Time) {
+		this.work3Time = work3Time;
+	}
+
+	public String getWork3Place() {
+		return work3Place;
+	}
+
+	public void setWork3Place(String work3Place) {
+		this.work3Place = work3Place;
+	}
+
+	public String getWork3Reason() {
+		return work3Reason;
+	}
+
+	public void setWork3Reason(String work3Reason) {
+		this.work3Reason = work3Reason;
+	}
+
+	public String getWork3Job() {
+		return work3Job;
+	}
+
+	public void setWork3Job(String work3Job) {
+		this.work3Job = work3Job;
+	}
+
+	public String getWork3Zyyj() {
+		return work3Zyyj;
+	}
+
+	public void setWork3Zyyj(String work3Zyyj) {
+		this.work3Zyyj = work3Zyyj;
+	}
+
+	public String getWork4Time() {
+		return work4Time;
+	}
+
+	public void setWork4Time(String work4Time) {
+		this.work4Time = work4Time;
+	}
+
+	public String getWork4Place() {
+		return work4Place;
+	}
+
+	public void setWork4Place(String work4Place) {
+		this.work4Place = work4Place;
+	}
+
+	public String getWork4Reason() {
+		return work4Reason;
+	}
+
+	public void setWork4Reason(String work4Reason) {
+		this.work4Reason = work4Reason;
+	}
+
+	public String getWork4Job() {
+		return work4Job;
+	}
+
+	public void setWork4Job(String work4Job) {
+		this.work4Job = work4Job;
+	}
+
+	public String getWork4Zyyj() {
+		return work4Zyyj;
+	}
+
+	public void setWork4Zyyj(String work4Zyyj) {
+		this.work4Zyyj = work4Zyyj;
+	}
+
+	public String getWork5Time() {
+		return work5Time;
+	}
+
+	public void setWork5Time(String work5Time) {
+		this.work5Time = work5Time;
+	}
+
+	public String getWork5Place() {
+		return work5Place;
+	}
+
+	public void setWork5Place(String work5Place) {
+		this.work5Place = work5Place;
+	}
+
+	public String getWork5Reason() {
+		return work5Reason;
+	}
+
+	public void setWork5Reason(String work5Reason) {
+		this.work5Reason = work5Reason;
+	}
+
+	public String getWork5Job() {
+		return work5Job;
+	}
+
+	public void setWork5Job(String work5Job) {
+		this.work5Job = work5Job;
+	}
+
+	public String getWork5Zyyj() {
+		return work5Zyyj;
+	}
+
+	public void setWork5Zyyj(String work5Zyyj) {
+		this.work5Zyyj = work5Zyyj;
+	}
+
+	public String getWork6Time() {
+		return work6Time;
+	}
+
+	public void setWork6Time(String work6Time) {
+		this.work6Time = work6Time;
+	}
+
+	public String getWork6Place() {
+		return work6Place;
+	}
+
+	public void setWork6Place(String work6Place) {
+		this.work6Place = work6Place;
+	}
+
+	public String getWork6Reason() {
+		return work6Reason;
+	}
+
+	public void setWork6Reason(String work6Reason) {
+		this.work6Reason = work6Reason;
+	}
+
+	public String getWork6Job() {
+		return work6Job;
+	}
+
+	public void setWork6Job(String work6Job) {
+		this.work6Job = work6Job;
+	}
+
+	public String getWork6Zyyj() {
+		return work6Zyyj;
+	}
+
+	public void setWork6Zyyj(String work6Zyyj) {
+		this.work6Zyyj = work6Zyyj;
 	}
 
 	public void setPdpSjA(String pdpSjA) {
