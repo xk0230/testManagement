b04dbcdd8f64e0c5166a47cc5e93b53b774d831b
丁振华 <391320750@qq.com>
2018-05-01 14:57:04 +0800

    借还书

--- WebContent/WEB-INF/pages/admin/book/bookManager.jsp
+++ WebContent/WEB-INF/pages/admin/book/bookManager.jsp
@@ -6,110 +6,87 @@
 </head>
 <body  >
 		<%@ include file="../../common/header.jsp"%>
-		<%@ include file="../../common/side.jsp"%>
 		<!-- Start #content -->
 		<div id="content" ng-app = "myApp" ng-controller="CostController as vm">
-			<!-- Start .content-wrapper -->
-			<div class="content-wrapper">
+		  <div class="main-inner">
+			<div class="container">
 				<div class="row">
-					<div class="col-lg-12 heading">
-                        <h1 class="page-header"><i class="st-chart"></i> 图书管理</h1>
-                        <!-- Start .bredcrumb -->
-                        <ul id="crumb" class="breadcrumb">
-                        </ul>
-					</div>
-				</div>
-				<div class="outlet">
-					<!-- Start .outlet -->
-					<!-- Page start here ( usual with .row ) -->
-					<div class="row">
-						<div class="col-lg-12">
-							<!-- col-lg-12 start here -->
-							<div class="panel panel-default toggle">
-								<!-- Start .panel -->
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"></h3>
-                                </div>
-								<div class="panel-body">
-									<div class="form-horizontal group-border" role="form">
-										<div class="form-group">
-											<div class="col-lg-3">
-												<label class="col-lg-4 control-label">图书名称</label>
-												<div class="col-lg-8">
-													<input type="text" class="form-control" name="name" ng-model="name" id="name" />
-												</div>
-											</div>
-											
-											<div class="col-lg-3">
-											</div>
-											<div class="col-lg-3">
-												<div class="col-lg-6">
-													<input type="button"  class="btn btn-lg btn-primary col-lg-12" name="query" ng-click="vm.getBookList()" value="查询" />
-												</div>
-												<div class="col-lg-6">
-													<input type="button" ng-if="${adminUser.position == 'ADMIN'}" class="btn btn-lg btn-success col-lg-12" name="query" ng-click="vm.addCost('', '.outlet')" value="新增图书" />
-												</div>
-											</div>
+					<div class="span12">
+						<div class="widget">
+							<!-- 标题 -->
+							<div class="widget-header">
+								<i class="icon-pushpin"></i>
+								<h3>图书管理</h3>
+							</div>
+							
+							<div class="widget-content" style="padding-bottom: 100px;">
+								<div class="row">
+									<div class="span4" style="height:37px;">
+										<span class="searchSpan pull-left">图书名称:</span>
+										<span class="pull-left">
+											<input type="text" class="form-control" name="name" ng-model="name" id="name" />
+										</span>
+									</div>
+									<div class="span1 pull-right"><input type="button"  class="btn btn-large btn-success btn-support-ask" name="query" ng-click="vm.getBookList()" value="查询" /></div>
+								</div>
+								<hr>
+								<!-- 查询结果 -->
+								<div class="row">
+									<div class="span12">
+										<div style="width:1134px;">
+											<button class="btn btn-invert" ng-click="vm.add()" ng-if="${adminUser.position == 'ADMIN'}"><i class="icon-plus"></i>新增</button>
 										</div>
-										
-										
+										<table class="table table-condensed table-bordered table-striped" style="width:1134px;margin-top:7px;" >
+											<thead >
+												<tr>
+												<th width="110px">操作</th>
+												<th>序号</th>
+												<th>图书编号</th>
+												<th>图书名称</th>
+												<th>当前借阅人</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr class="odd gradeX" ng-repeat="item in vm.list" ng-switch="item.editMode">
+													<!-- view -->
+													<td ng-switch-when="view">
+														<a href="javascript:;" class="btn btn-small" ng-click="vm.edit(item)" ng-if="${adminUser.position == 'ADMIN'}"><i class="icon-edit"></i></a>
+														<a href="javascript:;" class="btn btn-small btn-danger"  ng-click="vm.pullOrBackBook(item)" >借还</a>
+													</td>
+													<td ng-switch-when="view"><p ng-bind="$index + 1"></p></td>
+													<td ng-switch-when="view"><p ng-bind="item.bookNo"></p></td>
+													<td ng-switch-when="view"><p ng-bind="item.name"></p></td>
+													<td ng-switch-when="view"><p ng-bind="item.userName"></p></td>
+ 													<!-- edit -->
+													<td ng-switch-when="edit">
+														<a href="javascript:;" class="btn btn-small btn-success" ng-click="vm.save(item)"><i class="icon-ok"></i></a>
+													</td>
+													<td ng-switch-when="edit"><p ng-bind="$index + 1"></p></td>
+													<td ng-switch-when="edit">
+														<p ng-bind="item.bookNo"></p>
+													</td>
+													<td ng-switch-when="edit">
+														<input type="text" class="form-control" ng-model="item.name" />
+													</td>
+													<td ng-switch-when="edit">
+														<p ng-bind="item.userName"></p>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+										<div class="g-no-content" ng-if="vm.list && vm.list.length === 0">没有相关数据</div>
+										<div style="width:1134px;">
+											<%@ include file="../../common/page.jsp"%>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<!-- End .row -->
-				<div class="outlet">
-					<!-- Start .outlet -->
-					<!-- Page start here ( usual with .row ) -->
-					<div class="row">
-						<div class="col-lg-12">
-							<!-- col-lg-12 start here -->
-							<div class="panel panel-default toggle">
-								<!-- Start .panel -->
-								<div class="panel-heading">
-									<h3 class="panel-title"><i class="ec-list"></i>查询结果</h3>
-								</div>
-								<div class="panel-body">
-									<table class="table display" id="datatable">
-										<thead>
-											<tr>
-												<th>图书编号</th>
-												<th>图书名称</th>
-												<th>当前借阅人</th>
-												<th>操作</th>
-											</tr>
-										</thead>  
-										<tbody>
-											<tr class="odd gradeX" ng-repeat="item in vm.list">
-												
-												<td><p ng-bind="item.bookNo"></p></td>
-												<td><p ng-bind="item.name"></p></td>
-												<td><p ng-bind="item.userName"></p></td>
-												<td>
-													<button ng-show="${adminUser.position == 'ADMIN'}" type="button" class="btn btn-xs btn-success" ng-click="vm.editBook(item.id,'.outlet')"><i class="fa-edit"></i></button>
-													<button type="button" class="btn btn-xs btn-success" ng-click="vm.pullOrBackBook(item.id,'.outlet')"><i class="im-notebook"></i></button>
-												</td>
-												
-											</tr>
-										</tbody>
-									</table>
-									<div class="g-no-content" ng-if="vm.list && vm.list.length === 0">没有相关数据</div>
-									<%@ include file="../../common/page.jsp"%>
-								</div>
-
-						</div>
-						<!-- col-lg-12 end here -->
-					</div>
-					<!-- Page End here -->
-				</div>
-				<!-- End .outlet -->
 			</div>
-			<!-- End .content-wrapper -->
-			<div class="clearfix"></div>
 		</div>
-		<!-- End #content -->
+
 	<!-- 这里可以定义一个js文件放到public下面 -->
     <script type="text/ng-template" id="myModalAddContent.html">
 			<div class="modal-header">
--- WebContent/public/js/pages/book/script.js
+++ WebContent/public/js/pages/book/script.js
@@ -10,25 +10,7 @@ myAppModule.controller('CostController',
 		$scope.itemsPerPage = 50;
 		
 		this.$onInit = function(){
-			self.getDeparts();
-			$scope.mode = $location.search().mode;
-			$scope.costTypeList = [
-				{costType : "0", name : "收入"},
-				{costType : "1", name : "支出"}
-			];
-			
-			//获取角色
-			self.admin =$("#sessionUserType").val();
-			if(self.admin=="MANAGER"){
-				$scope.dep =$("#sessionUserDepId").val();
-				$scope.depIdChangeAble = true;
-			}else if(self.admin=="ADMIN"){
-				$scope.depIdChangeAble = false;
-			}
 			self.getBookList();
-			if($scope.mode == 'add'){
-				self.addCost('', '.outlet');
-			}
 		};
 		
 		$scope.setPage = function (pageNo) {
@@ -39,36 +21,8 @@ myAppModule.controller('CostController',
 			self.getBookList();
 		};
 		
-		$scope.costTypeChange = function() {
-			
-			var costTypeValue = $scope.costType;
-			if(costTypeValue == 0 || costTypeValue == 1){
-				
-				$http({
-					method:'POST',
-					url:$("#rootUrl").val()+"/admin/cost/subType/"+costTypeValue+".do",
-					params:{
-					}
-				
-				}).then(function(res){
-					
-					if(res.data.code == 0){
-						$scope.costSubTypeList = res.data.objData;
-					}else{
-						$scope.costSubTypeList = [];
-					}
-					
-				});
-				
-			}else{
-				$scope.costSubTypeList = [];
-			}
-			
-		};
-		
 		// 获取数据列表
 		this.getBookList = function(){
-			
 			$http({
 				method:'POST',
 				url:$("#rootUrl").val()+'/admin/book/page.do',
@@ -90,526 +44,78 @@ myAppModule.controller('CostController',
 			});
 		};
 		
-		this.getDeparts = function(){
+		/*上传*/
+		this.save = function (item) {
+			
+			if(!item){
+				swal("错误提示", "请填写完整信息！", "warning");
+				return ;
+			}
+			if(!item.name){
+				swal("错误提示", "请填写图书名称！", "warning");
+				return ;
+			}
+			
+			var params = {
+				id:item.id,
+				name:item.name
+			};
 			
 			$http({
 				method:'POST',
-				url:$("#rootUrl").val()+'/admin/dep/getAlldep.do',
-				params:{
-				}
-			
+				url:$("#rootUrl").val()+"/admin/book/saveOrUpdate.do",
+				params:params
 			}).then(function(res){
-				if(res){
-					$scope.depList = res.data || [];
+				if(res.data.code == 0){
+					swal(res.data.msg);
+					self.getBookList();
 				}else{
-					$scope.depList = [];
+					swal(res.data.msg);
 				}
 			});
+		};
+		
+		/*添加*/
+		this.add = function(){
+			var newItem = {
+				name:""
+				,editMode:"edit"
+			};
+			var myArray=new Array()
+			myArray.push(newItem);
 			
-			
-		};
+			$.each(self.list,
+				function(index, value) {
+					myArray.push(value);
+				}
+			);
+			self.list = myArray;
+		}
 		
-		//日期模块加载
-		$scope.today = function() {
-			$scope.dt = new Date();
-		};
-		$scope.clear = function() {
-			$scope.dt = null;
-		};
-
-		$scope.dateOptions = {
-			dateDisabled: "",
-			formatYear: 'yyyy',
-			maxDate: new Date(9999, 12, 31),
-			minDate: new Date(1000, 1,1),
-			startingDay: 1,
-		};
-
-		$scope.open = function(mode) {
-			if(mode == 1){
-				$scope.popup.opened1 = true;
-			}else if (mode == 2){
-				$scope.popup.opened2 = true;
-			}else if (mode == 3){
-				$scope.popup.opened3 = true;
-			}else if (mode == 4){
-				$scope.popup.opened4 = true;
-			}
-			
-		};
-	
-		$scope.popup = {
-			opened1: false,
-			opened2: false,
-			opened3: false,
-			opened4: false
-		};
+		/*编辑*/
+		this.edit = function(item){
+			item.editMode = "edit"
+		}
 		
-		this.addCost = function(costId, parentSelector){
-			var parentElem = parentSelector ? angular.element($document[0].querySelector('.content-wrapper ' + parentSelector)) : undefined;
-    	    var modalInstance = $uibModal.open({
-    	      animation: true,
-    	      ariaLabelledBy: 'modal-title',
-    	      ariaDescribedBy: 'modal-body',
-    	      templateUrl: 'myModalAddContent.html',
-    	      controller: 'ModalAddInstanceCtrl',
-    	      controllerAs: '$ctrl',
-    	      size: 'lg',
-    	      appendTo: parentElem,
-    	      //参数
-    	      resolve: {
-    	    	  //好像必须得这么写
-    	        items: function () {
-    	          return costId;
-    	        }
-    	      }
-    	    });
-
-    	    modalInstance.result.then(function (selectedItem) {
-    	    	//ok的回调函数
-    	    	if(selectedItem == '0'){
-    	    		self.getBookList();
-    	    	}
-    	    	
-    	    }, function () {
-    	    	//取消的回调函数
-    	    });
+		this.pullOrBackBook = function (item) {
+			$http({
+				method:'POST',
+				url:$("#rootUrl").val()+"/admin/book/getOrBack/"+item.id+".do",
+				params:{}
+			}).then(function(res){
+				
+				if(res.data.code == 0){
+					swal(res.data.msg);
+					self.getBookList();
+				}else{
+					swal(res.data.msg);
+				}
+				
+			});
 		};
-		
-		
-		this.editBook = function (costId, parentSelector) {
-		    var parentElem = parentSelector ? angular.element($document[0].querySelector('.content-wrapper ' + parentSelector)) : undefined;
-		    	    var modalInstance = $uibModal.open({
-		    	      animation: true,
-		    	      ariaLabelledBy: 'modal-title',
-		    	      ariaDescribedBy: 'modal-body',
-		    	      templateUrl: 'myModalEditContent.html',
-		    	      controller: 'ModalInstanceCtrl',
-		    	      controllerAs: '$ctrl',
-		    	      size: 'lg',
-		    	      appendTo: parentElem,
-		    	      //参数
-		    	      resolve: {
-		    	    	  //好像必须得这么写
-		    	        items: function () {
-		    	          return costId;
-		    	        }
-		    	      }
-		    	    });
-
-		    	    modalInstance.result.then(function (selectedItem) {
-		    	    	
-		    	    	//ok的回调函数
-		    	    	if(selectedItem == '0'){
-		    	    		self.getBookList();
-		    	    	}
-		    	    	
-		    	    }, function () {
-		    	    	//取消的回调函数
-		    	    	
-		    	    });
-		   };
-		    	  
-		   this.pullOrBackBook = function (costId, parentSelector) {
-			    var parentElem = parentSelector ? angular.element($document[0].querySelector('.content-wrapper ' + parentSelector)) : undefined;
-			    	    var modalInstance = $uibModal.open({
-			    	      animation: true,
-			    	      ariaLabelledBy: 'modal-title',
-			    	      ariaDescribedBy: 'modal-body',
-			    	      templateUrl: 'myModalDelContent.html',
-			    	      controller: 'ModalInstanceDel',
-			    	      controllerAs: '$ctrl',
-			    	      size: 'sm',
-			    	      appendTo: parentElem,
-			    	      //参数
-			    	      resolve: {
-			    	    	  //好像必须得这么写
-			    	        items: function () {
-			    	          return costId;
-			    	        }
-			    	      }
-			    	    });
-
-			    	    modalInstance.result.then(function (selectedItem) {
-			    	    	
-			    	    	//ok的回调函数
-			    	    	if(selectedItem == '0'){
-			    	    		self.getBookList();
-			    	    	}
-			    	    	
-			    	    }, function () {
-			    	    	//取消的回调函数
-			    	    });
-			   };
 		
 	}
 );
 
-//添加页面的control
-angular.module('myApp').controller('ModalAddInstanceCtrl', 
-		function ($scope,$http,$uibModalInstance,$filter, items) {
-	
-	var $ctrl = this;
-	
-	this.$onInit = function(){
-		 $ctrl.getDeparts();
-	};
-	
-	this.getDeparts = function(){
-		
-		/*$http({
-			method:'POST',
-			url:$("#rootUrl").val()+'/admin/dep/getAlldep.do',
-			params:{
-			}
-		
-		}).then(function(res){
-			if(res){
-				var depList = res.data;
-				var html = "";
-				for(var i = 0;i<depList.length;i++){
-					
-					if(i % 2 == 0){
-						if(html != ''){
-							html += '</div>';
-						}
-						
-						html += '<div class="form-group">';
-					}
-					
-					html += '<div class="col-lg-6">';
-					html += '<label class="col-lg-4 control-label">';
-					html += depList[i].name;
-					html += '-金额</label>';
-					html += '<div class="col-lg-8">';
-					html += '<input type="hidden" name="depIds" class="form-control" value="'+depList[i].depId+'"/>';
-					html += '<input type="text" name="depNums" class="form-control" />';
-					html += '</div></div>';
-					
-				}
-				
-				html += '</div>';
-				
-				html += '<div class="form-group">';
-				html += '<div class="col-lg-6">';
-				html += '<label class="col-lg-4 control-label">成本详情</label>';
-				html += '<div class="col-lg-8">';
-				html += '<textarea class="form-control" id="remark" rows="3" Placeholder="请输入成本详情"></textarea>';
-				html += '</div></div></div>';
-				
-				$('#addDepNum').append(html);
-			}
-		});
-		*/
-	};
-	
-	$scope.costTypeChange = function() {
-		
-		var costTypeValue = $scope.bookEntity.costType;
-		if(costTypeValue == 0 || costTypeValue == 1){
-			
-			$http({
-				method:'POST',
-				url:$("#rootUrl").val()+"/admin/cost/subType/"+costTypeValue+".do",
-				params:{
-				}
-			
-			}).then(function(res){
-				
-				if(res.data.code == 0){
-					$scope.costSubTypeList = res.data.objData;
-					
-				}else{
-					$scope.costSubTypeList = [];
-				}
-				
-			});
-			
-		}else{
-			$scope.costSubTypeList = [];
-		}
-		
-	};
-	
-	$ctrl.ok = function () {
-		  if(!$scope.bookEntity){
-			  alert("请填写完整信息");
-			  return ;
-		  }
-		  
-		  if(!$scope.bookEntity.name){
-			  alert("请填写图书名称");
-			  return ;
-		  }
-		  
-		 var params = {
-				name:$scope.bookEntity.name
-			};
-		 
-		 $http({
-				method:'POST',
-				url:$("#rootUrl").val()+"/admin/book/saveOrUpdate.do",
-				params:params
-			
-			}).then(function(res){
-				
-				if(res.data.code == 0){
-					$uibModalInstance.close('0');
-				}
-			});
-	  };
-
-	
-	 $ctrl.cancel = function () {
-		  $uibModalInstance.dismiss('cancel');
-     };
-     
-     //日期模块加载
-	$scope.today = function() {
-		$scope.dt = new Date();
-	};
-	$scope.clear = function() {
-		$scope.dt = null;
-	};
-
-	$scope.dateOptions = {
-		dateDisabled: "",
-		formatYear: 'yyyy',
-		maxDate: new Date(9999, 12, 31),
-		minDate: new Date(1000, 1,1),
-		startingDay: 1,
-	};
-
-	$scope.open = function(mode) {
-		if(mode == 1){
-			$scope.popup.opened1 = true;
-		}else if (mode == 2){
-			$scope.popup.opened2 = true;
-		}else if (mode == 3){
-			$scope.popup.opened3 = true;
-		}else if (mode == 4){
-			$scope.popup.opened4 = true;
-		}
-		
-	};
-
-	$scope.popup = {
-		opened1: false,
-		opened2: false,
-		opened3: false,
-		opened4: false
-	};
-	
-});
-
-//编辑页面的control
-angular.module('myApp').controller('ModalInstanceCtrl', 
-		function ($scope,$http,$uibModalInstance,$filter, items) {
-	  var $ctrl = this;
-	  $ctrl.items = items;
-	  $ctrl.bookEntity = {
-				
-			};
-	  
-	  this.$onInit = function(){
-		  
-		  if(items != ''){
-			  
-			  $http({
-					method:'POST',
-					url:$("#rootUrl").val()+"/admin/book/get/"+items+".do",
-					params:{
-					}
-				
-				}).then(function(res){
-					
-					if(res.data.code == 0){
-						
-						$scope.bookEntity = res.data.objData;
-						
-						/*$http({
-							method:'POST',
-							url:$("#rootUrl").val()+"/admin/cost/subType/"+res.data.objData.costType+".do",
-							params:{
-							}
-						
-						}).then(function(res){
-							
-							if(res.data.code == 0){
-								$scope.costSubTypeList = res.data.objData;
-							}else{
-								$scope.costSubTypeList = [];
-							}
-							
-						});*/
-					}
-				});
-		  }
-		
-		  //$ctrl.getDeparts();
-		  
-	  };
-		
-	  $ctrl.selected = {
-	    item: $ctrl.items[0]
-	  };
-
-	  $ctrl.ok = function () {
-		  
-		  if(!$scope.bookEntity){
-			  alert("请填写完整信息");
-			  return ;
-		  }
-		  if(!$scope.bookEntity.name){
-			  alert("请填写图书名称");
-			  return ;
-		  }
-		 var params = {
-				name:$scope.bookEntity.name,
-				id:$scope.bookEntity.id
-			}; 
-		 
-		 $http({
-				method:'POST',
-				url:$("#rootUrl").val()+"/admin/book/saveOrUpdate.do",
-				params:params
-			
-			}).then(function(res){
-				
-				if(res.data.code == 0){
-					$uibModalInstance.close('0');
-				}
-				
-			});
-		  
-	    
-	  };
-
-	  $ctrl.cancel = function () {
-	    $uibModalInstance.dismiss('cancel');
-	  };
-	  
-	  this.getDeparts = function(){
-			
-			$http({
-				method:'POST',
-				url:$("#rootUrl").val()+'/admin/dep/getAlldep.do',
-				params:{
-				}
-			
-			}).then(function(res){
-				if(res){
-					$scope.depList = res.data || [];
-				}else{
-					$scope.depList = [];
-				}
-			});
-			
-	  };
-	  
-	  $scope.costTypeChange = function() {
-			
-			var costTypeValue = $scope.bookEntity.costType;
-			if(costTypeValue == 0 || costTypeValue == 1){
-				
-				$http({
-					method:'POST',
-					url:$("#rootUrl").val()+"/admin/cost/subType/"+costTypeValue+".do",
-					params:{
-					}
-				
-				}).then(function(res){
-					
-					if(res.data.code == 0){
-						$scope.costSubTypeList = res.data.objData;
-						
-					}else{
-						$scope.costSubTypeList = [];
-					}
-					
-				});
-				
-			}else{
-				$scope.costSubTypeList = [];
-			}
-			
-		};
-		
-		//日期模块加载
-		$scope.today = function() {
-			$scope.dt = new Date();
-		};
-		$scope.clear = function() {
-			$scope.dt = null;
-		};
-
-		$scope.dateOptions = {
-			dateDisabled: "",
-			formatYear: 'yyyy',
-			maxDate: new Date(9999, 12, 31),
-			minDate: new Date(1000, 1,1),
-			startingDay: 1,
-		};
-
-		$scope.open = function(mode) {
-			if(mode == 1){
-				$scope.popup.opened1 = true;
-			}else if (mode == 2){
-				$scope.popup.opened2 = true;
-			}else if (mode == 3){
-				$scope.popup.opened3 = true;
-			}else if (mode == 4){
-				$scope.popup.opened4 = true;
-			}
-			
-		};
-	
-		$scope.popup = {
-			opened1: false,
-			opened2: false,
-			opened3: false,
-			opened4: false
-		};
-	  
-	});
-
-
-//借书页面的control
-angular.module('myApp').controller('ModalInstanceDel', 
-		function ($scope,$http,$uibModalInstance,items) {
-	  var $ctrl = this;
-	  $ctrl.items = items;
-	  
-	  $ctrl.selected = {
-	    item: $ctrl.items[0]
-	  };
-
-	  $ctrl.ok = function () {
-		  $http({
-				method:'POST',
-				url:$("#rootUrl").val()+"/admin/book/getOrBack/"+items+".do",
-				params:{}
-			
-			}).then(function(res){
-				
-				if(res.data.code == 0){
-					$uibModalInstance.close('0');
-					swal(res.data.msg,"","success");
-				}else{
-					$uibModalInstance.close('0');
-					swal(res.data.msg,"","error");
-				}
-				
-			});
-		  
-		  //$uibModalInstance.close('0');
-	    
-	  };
-
-	  $ctrl.cancel = function () {
-	    $uibModalInstance.dismiss('cancel');
-	  };
-	  
-	});
 
 angular.bootstrap(document.getElementById("content"), ['myApp']);
